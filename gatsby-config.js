f9e1c6b974261a48742fb590857308e5e5669834
Andrew Davisson <akdavisson4@gmail.com>
2020-09-19 20:39:16 -0600

    updated with my details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Andrew Davisson`,
+    description: `Andrew Davisson's Portfolio Website`,
+    author: 'Andrew Davisson'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
